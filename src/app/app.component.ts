5c900cd849f92dcf0b56db5cdd70210db15a3980
ravi <ravish1086@gmail.com>
2026-01-27 22:37:18 +0530

    feat: Initialize sweet shop e-commerce application with core UI components, styling, and assets.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,59 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'sweetApp';
+  title = 'Brijbhog';
+
+  products = [
+    {
+      id: 1,
+      name: 'Kaju Katli',
+      price: 800,
+      image: 'assets/kaju_katli.png',
+      badge: 'Bestseller'
+    },
+    {
+      id: 2,
+      name: 'Gulab Jamun',
+      price: 450,
+      image: 'assets/gulab_jamun.png',
+      badge: 'Fresh'
+    },
+    {
+      id: 3,
+      name: 'Motichoor Laddu',
+      price: 400,
+      image: 'assets/motichoor_laddu.png',
+      badge: ''
+    },
+    {
+      id: 4,
+      name: 'Rasgulla',
+      price: 350,
+      image: 'assets/rasgulla.png',
+      badge: 'Sale'
+    },
+     {
+      id: 5,
+      name: 'Besan Laddu',
+      price: 380,
+      image: 'assets/motichoor_laddu.png',
+      badge: ''
+    },
+    {
+      id: 6,
+      name: 'Milk Cake',
+      price: 550,
+      image: 'assets/kaju_katli.png',
+      badge: 'Premium'
+    }
+  ];
 }
